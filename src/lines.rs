8f09dfe3e3a82e64424b377c5f764ab0d7b27efc
DGriffin91 <github@dgdigital.net>
2024-08-05 13:00:06 -0700

    clean up

--- src/lines.rs
+++ src/lines.rs
@@ -2,21 +2,14 @@
 // From https://github.com/bevyengine/bevy/blob/0c7df881e72c1167060c170b09e02586fc74d85f/examples/3d/lines.rs#L1
 
 use bevy::{
-    pbr::{MaterialPipeline, MaterialPipelineKey},
     prelude::*,
     reflect::TypePath,
     render::{
-        mesh::{MeshVertexBufferLayoutRef, PrimitiveTopology},
+        mesh::PrimitiveTopology,
         render_asset::RenderAssetUsages,
-        render_resource::{
-            AsBindGroup, PolygonMode, RenderPipelineDescriptor, ShaderRef,
-            SpecializedMeshPipelineError,
-        },
+        render_resource::{AsBindGroup, ShaderRef},
     },
 };
-
-/// This example uses a shader source file from the assets subdirectory
-const SHADER_ASSET_PATH: &str = "shaders/line_material.wgsl";
 
 #[derive(Asset, TypePath, Default, AsBindGroup, Debug, Clone)]
 pub struct LineMaterial {
@@ -26,18 +19,7 @@ pub struct LineMaterial {
 
 impl Material for LineMaterial {
     fn fragment_shader() -> ShaderRef {
-        SHADER_ASSET_PATH.into()
-    }
-
-    fn specialize(
-        _pipeline: &MaterialPipeline<Self>,
-        descriptor: &mut RenderPipelineDescriptor,
-        _layout: &MeshVertexBufferLayoutRef,
-        _key: MaterialPipelineKey<Self>,
-    ) -> Result<(), SpecializedMeshPipelineError> {
-        // This is the important part to tell bevy to render this material as a line between vertices
-        descriptor.primitive.polygon_mode = PolygonMode::Line;
-        Ok(())
+        "shaders/line_material.wgsl".into()
     }
 }
 
@@ -50,33 +32,10 @@ pub struct LineList {
 impl From<LineList> for Mesh {
     fn from(line: LineList) -> Self {
         let vertices: Vec<_> = line.lines.into_iter().flat_map(|(a, b)| [a, b]).collect();
-
-        Mesh::new(
-            // This tells wgpu that the positions are list of lines
-            // where every pair is a start and end point
-            PrimitiveTopology::LineList,
-            RenderAssetUsages::RENDER_WORLD,
-        )
-        // Add the vertices positions as an attribute
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
-    }
-}
-
-/// A list of points that will have a line drawn between each consecutive points
-#[derive(Debug, Clone)]
-struct LineStrip {
-    points: Vec<Vec3>,
-}
-
-impl From<LineStrip> for Mesh {
-    fn from(line: LineStrip) -> Self {
-        Mesh::new(
-            // This tells wgpu that the positions are a list of points
-            // where a line will be drawn between each consecutive point
-            PrimitiveTopology::LineStrip,
-            RenderAssetUsages::RENDER_WORLD,
-        )
-        // Add the point positions as an attribute
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, line.points)
+        // This tells wgpu that the positions are list of lines
+        // where every pair is a start and end point
+        Mesh::new(PrimitiveTopology::LineList, RenderAssetUsages::RENDER_WORLD)
+            // Add the vertices positions as an attribute
+            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
     }
 }
